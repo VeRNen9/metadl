51f17a77df0f9540f10c6a76d0e1e5a8d896d7ce
andr2xea <andreea.d135@gmail.com>
2020-11-03 21:36:09 +0100

    Update embedding to 4 stack and flatten output.

--- matching_networks/helper.py
+++ matching_networks/helper.py
@@ -1,29 +1,37 @@
 import tensorflow as tf
 
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, \
-     BatchNormalization, ReLU
+from tensorflow.keras.layers import Conv2D, BatchNormalization, \
+            ReLU, Flatten, MaxPooling2D
 
 
 tf.random.set_seed(1234)
 def f_embedding(img_size=28, embedding_dim=64):
     model = Sequential()
     
-    model.add(Conv2D(embedding_dim, (3, 3),
+    model.add(Conv2D(embedding_dim, (3, 3), padding='same',
                      input_shape=(img_size, img_size, 3)))
     model.add(BatchNormalization())
     model.add(ReLU())
     model.add(MaxPooling2D((2, 2)))
 
-    model.add(Conv2D(embedding_dim, (3, 3)))
+    model.add(Conv2D(embedding_dim, (3, 3), padding='same'))
     model.add(BatchNormalization())
     model.add(ReLU())
     model.add(MaxPooling2D((2, 2)))
 
-    model.add(Conv2D(embedding_dim, (3, 3)))
+    model.add(Conv2D(embedding_dim, (3, 3), padding='same'))
     model.add(BatchNormalization())
     model.add(ReLU())
     model.add(MaxPooling2D((2, 2)))
     
+    model.add(Conv2D(embedding_dim, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(ReLU())
+    model.add(MaxPooling2D((2, 2)))
+    
+    model.add(Flatten())
+    
     model._name = 'F_embedding'
+
     return model
