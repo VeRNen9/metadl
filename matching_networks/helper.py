fde212db2aa9e421afec9e38195b461147ca2063
andr2xea <andreea.d135@gmail.com>
2020-11-04 13:28:23 +0100

    Add docstrings.

--- matching_networks/helper.py
+++ matching_networks/helper.py
@@ -7,6 +7,22 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization, \
 
 tf.random.set_seed(1234)
 def f_embedding(img_size=28, embedding_dim=64):
+    """
+    Implements embedding function for the query set.
+    
+    Parameters
+    ----------
+    img_size: int
+        Size of the input image. Images expected to have
+        the width and height equal.
+    embedding_dim: int
+        Dimension of the embedding for one image.
+        
+    Returns
+    -------
+    tensorflow.python.keras.engine.sequential.Sequential
+        Sequential tensorflow architecture.
+    """
     model = Sequential()
     
     model.add(Conv2D(embedding_dim, (3, 3), padding='same',
