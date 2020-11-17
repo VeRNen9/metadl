81796ef44fa2577c789031aa6d02685216584550
andr2xea <andreea.d135@gmail.com>
2020-11-17 13:37:20 +0100

    Add matching networks.

--- matching_networks/helper.py
+++ matching_networks/helper.py
@@ -8,59 +8,35 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization, \
 tf.random.set_seed(1234)
 
 
-def embedding_architecture(img_size=28, num_channels=3,
-                           embedding_dim=64, name=None):
+def embedding_architecture(input_set, embedding_dim):
     """
     Implements embedding function architecture.
-    
-    Parameters
-    ----------
-    img_size: int
-        Size of the input image. Images expected to have
-        the width and height equal.
-    num_channels: int
-        Number of channels in the input images.
-    embedding_dim: int
-        Dimension of the embedding for one image.
-    name: str
-        Name to be assigned to the model.
-        
-    Returns
-    -------
-    tensorflow.python.keras.engine.sequential.Sequential
-        Sequential tensorflow architecture.
     """
-    model = Sequential()
-    
+    x = input_set
     for layer in range(4):
         conv_params = {
             'filters': embedding_dim,
             'kernel_size': (3, 3),
             'padding': 'same'
         }
-        if layer == 0:
-            conv_params['input_shape'] = (img_size, img_size, num_channels)
+        x = Conv2D(**conv_params)(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = MaxPooling2D((2, 2))(x)
 
-        model.add(Conv2D(**conv_params))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling2D((2, 2)))
-
-    model.add(Flatten())
-    if name:
-        model._name = name
-    return model
+    x = Flatten()(x)
+    return x
 
 
 def cosine_distance(x, y):
     """
     Implements the cosine distance between two tensors.
     """
-    return 1 - tf.keras.losses.cosine_similarity(x, y)
+    return 1 - (-1 * tf.keras.losses.cosine_similarity(
+        tf.expand_dims(x, 1), y))
 
 
-def attention_mechanism(f_embedding, g_embedding,
-                        distance_func=cosine_distance):
+def attention_mechanism(f, g, distance_func=cosine_distance):
     """
     Implements the attention mechanism. Computes the cosine distance
     between each image embedding in the query set and the embeddings
@@ -69,9 +45,9 @@ def attention_mechanism(f_embedding, g_embedding,
     
     Parameters
     ----------
-    f_embeddings: tensorflow.python.framework.ops.EagerTensor
+    f: tf.Tensor
         Embeddings for the images in the query set.
-    g_embeddings: tensorflow.python.framework.ops.EagerTensor
+    g: tf.Tensor
         Embeddings for the images in the support set.
     distance_func: function
         Function that implements the distance measure to be
@@ -81,10 +57,7 @@ def attention_mechanism(f_embedding, g_embedding,
         
     Returns
     -------
-    tensorflow.python.framework.ops.EagerTensor
+    tf.Tensor
     A probability distributions for each image in the query set.
     """
-    similarities = [distance_func(embedded_img, g_embedding)
-                    for embedding_img in f_embedding]
-    similarities = tf.stack(similarities)
-    return tf.nn.softmax(similarities, axis=1)
+    return tf.nn.softmax(distance_func(f, g), name='attention_mechanism')
--- matching_networks/model.py
+++ matching_networks/model.py
@@ -1,11 +1,12 @@
+import csv
+import gin
 import os
 import logging
-import csv 
 
 import tensorflow as tf
 
 from metadl.api.api import MetaLearner, Learner, Predictor
-from helper import embedding_architecture, cosine_distance, attention_mechanism
+from .helper import embedding_architecture, cosine_distance, attention_mechanism
 
 
 # Line necessary for duranium. Comment it otherwise.
@@ -14,6 +15,10 @@ tf.random.set_seed(1234)
 
 @gin.configurable
 class MyMetaLearner(MetaLearner):
+    
+    distance_functions = {
+        'cosine': cosine_distance
+    }
 
     def __init__(self, img_size, num_channels, n_way=5, k_shot=1,
                  embedding_dim=64, distance_func='cosine'):
@@ -25,30 +30,34 @@ class MyMetaLearner(MetaLearner):
         self.embedding_dim = embedding_dim
         self.distance_func = self._set_distance_func(distance_func)
         
-        self.f = embedding_architecture(
-            img_size=img_size,
-            num_channels=num_channels,
-            embedding_dim=embedding_dim,
-            name='f_embedding')
-        
-        self.g = embedding_architecture(
-            img_size=img_size,
-            num_channels=num_channels,
-            embedding_dim=embedding_dim,
-            name='g_embedding')
-        
-        self.attention = attention_mechanism(
-            f_embedding=f,
-            g_embedding=g,
-            distance_func=self.distance_func)
+        self.model = self._matching_architecture()
 
-        self.classifier = None
-        
     def _set_distance_func(self, distance_func):
-            if distance_func == 'cosine':
-                self.distance_func = cosine_distance
-            else:
-                raise ValueError('Only cosine distance supported.')
+        if distance_func not in MyMetaLearner.distance_functions:
+            raise ValueError(
+                'Distance functions not supported.\n' +
+                'Available functions: ' +
+                ', '.join(list(MyMetaLearner.distance_functions.keys())))
+        return MyMetaLearner.distance_functions[distance_func]
+
+    def _matching_architecture(self):
+        support_set = tf.keras.Input(
+            shape=(self.img_size, self.img_size, self.num_channels, ))
+        query_set = tf.keras.Input(
+            shape=(self.img_size, self.img_size, self.num_channels, ))
+
+        f = embedding_architecture(query_set,
+                                   embedding_dim=self.embedding_dim)
+        g = embedding_architecture(support_set,
+                                   embedding_dim=self.embedding_dim)
+        
+        attention = attention_mechanism(
+            f, g, distance_func=self.distance_func)
+        
+        model = tf.keras.Model(inputs=[support_set, query_set],
+                               outputs=attention)
+        model._name = 'matching_networks'
+        return model
 
     def meta_fit(self, meta_dataset_generator) -> Learner:
         raise NotImplemented()
@@ -67,7 +76,7 @@ class MyLearner(Learner):
             
     def load(self, model_dir):
         raise NotImplemented()
-        
+
     
 class MyPredictor(Predictor):
 
